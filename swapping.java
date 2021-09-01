0bb988562eee7c05f338f258eb76ebb44b4b207e
puru9211 <89893278+puru9211@users.noreply.github.com>
2021-09-01 10:15:50 +0530

    Create swapping.java

---
+++ swapping.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Swap_With {
+
+public static void main(String[] args) {
+
+int x, y, t;
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the value of X and Y");
+
+x = sc.nextInt();
+
+y = sc.nextInt();
+
+System.out.println("before swapping numbers: "+x +" "+ y);
+
+t = x;
+
+x = y;
+
+y = t;
+
+System.out.println("After swapping: "+x +" " + y);
+
+System.out.println( );
+
+}
+
+}
